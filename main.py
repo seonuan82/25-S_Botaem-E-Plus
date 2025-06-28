4b50a116d6cb86e6dda6f2c05ddbd9a148a37c94
Seonu An <150267997+seonuan82@users.noreply.github.com>
2025-06-29 03:00:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if 'user' not in st.session_state:
         if success:
             st.session_state['user'] = user
             st.success("로그인 성공!")
-            st.experimental_rerun()
+            st.stop()
         else:
             st.error("아이디 또는 비밀번호가 잘못되었습니다.")
     st.stop()  # 로그인 안됐으면 뒤 코드 실행 중단
