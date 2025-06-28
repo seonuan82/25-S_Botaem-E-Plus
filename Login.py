e199c56e72b98cb472b4a57b017ff9e000e2cbab
Seonu An <150267997+seonuan82@users.noreply.github.com>
2025-06-28 18:32:39 +0900

    Update and rename main.py to Login.py

---
+++ Login.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import time
+from utility.supabase import login_user
+
+def show_logo():
+    st.image("assets/logo.png", use_column_width=True)
+    time.sleep(3)
+
+def show_login_form():
+    st.title("로그인")
+    email = st.text_input("ID")
+    password = st.text_input("비밀번호", type="password")
+    if st.button("로그인"):
+        success, session = login_user(ID, password)
+        if success:
+            st.session_state['user'] = session
+            st.switch_page("pages/Main_Page.py")
+        else:
+            st.error("로그인 실패. 다시 시도해주세요.")
+
+if 'user' not in st.session_state:
+    show_logo()
+    show_login_form()
+else:
+    st.switch_page("pages/Main_Page.py")
